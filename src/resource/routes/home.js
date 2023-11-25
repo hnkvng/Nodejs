7f9c502035eba7633f93102b9c50bc96b4edd84b
H.N.K <huynhnamkha512020@gmail.com>
2023-11-25 19:06:43 +0700

    j

--- src/resource/routes/home.js
+++ src/resource/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Controllers = require('../controllers/homeController');
 
-router.get('/add', Controllers.add);
+router.get('/:slug', Controllers.add);
 router.get('/', Controllers.index);
 
 module.exports = router;
